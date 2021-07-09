0d92535d776ffc99117fc2bd9f541792cdf8bcb1
AJAY487-star <64407115+AJAY487-star@users.noreply.github.com>
2021-07-09 14:50:15 +0530

    Update permission.sh

--- scripts/permission.sh
+++ scripts/permission.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 chmod -R 755 /var/www/html/wp-content
+chown -R www-data:www-data /var/www/html/wp-content 
 chmod -R 755 /var/www/html/wp-content/uploads
 chown -R www-data:www-data /var/www/html/wp-content/uploads
